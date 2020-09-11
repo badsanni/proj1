c18efdbdcbaded8cbf86f043ecd914308fa9171a
badsanni <femidba14@gmail.com>
2020-09-10 22:27:40 -0400

    prof 1

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+}	
+resource "aws_instance" "helloworld" {
+  ami = "ami-0cebb45b34604efb8"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name       = "helloworld"
+    Created_by = "Terraform"
+
+  }
+}
